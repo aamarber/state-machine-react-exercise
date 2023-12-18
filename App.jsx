d77a28800d1557d91e7ec5e771f328a5f61438c5
melocotoneroh <aamarber@gmail.com>
2023-12-18 14:45:10 +0100

    Initial state

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import React from "react"
+import StateMachine from "./StateMachine/StateMachine";
+import './App.scss';
+
+function App() {
+
+    const states = [
+        {name: 'A'},
+        {name: 'B'},
+        {name: 'C'},
+        {name: 'D'},
+        {name: 'E'},
+    ];
+
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default App
